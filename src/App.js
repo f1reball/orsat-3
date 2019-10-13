9b3726fef9235684310faefecb732ae18d5d9f5e
Jackson Booth <jacksontbooth@icloud.com>
2019-10-13 23:59:05 +1100

    updated system detection

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import platform from 'platform';
+
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Chart from './components/Chart'
@@ -15,6 +17,16 @@ import Button from 'react-bootstrap/Button';
 
 function App() {
 const [modalShow, setModalShow] = React.useState(false);
+
+
+    //scan through all ports and find the ID for the scanner on both mac and Windows
+    //then connect to that port using windows or mac connection
+
+    if(platform.os.family == "Windows"){
+        console.log("fked");
+    }
+
+    console.log(platform.os);
 
   return (
     <div className="App">
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -16,7 +16,12 @@ class Chart extends Component{
                     {
                         label: 'Test label',
                         data:[1, 5, 3, 8],
-                        backgroundColor:['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)']
+                        pointBackgroundColor:['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)'],
+                        pointBorderColor:['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)'],
+                        borderColor: 'rgba(255, 206, 86, 1)',
+                        backgroundColor: 'rgba(75, 192, 192, 1)',
+                        fill: true,
+                        lineTension: '0.4'
                     }
                 ]
             }
@@ -33,7 +38,7 @@ class Chart extends Component{
                 options={{
                     maintainAspectRatio: true,
                     responsive: true,
-                    legend:{display: false}
+                    legend:{display: false},
                 }}
                 />
             </div>
